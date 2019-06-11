cd8caf08e823d0c4a52b6beb6e7627d169303605
acsb <acsb@cin.ufpe.br>
2019-06-11 15:55:36 -0300

    [FEATURE 13] Create Profile & Solve App Name's problem
    added a fragment to implement the profile. Solving the problem about the app name, it was
    appearing LobbyActivity on app name's place.

--- app/src/main/java/com/discordtime/gpts/lobby/LobbyActivity.kt
+++ app/src/main/java/com/discordtime/gpts/lobby/LobbyActivity.kt
@@ -3,22 +3,25 @@ package com.discordtime.gpts.lobby
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.FragmentManager
 import com.discordtime.gpts.R
 import com.discordtime.gpts.listplaces.view.ListPlacesFragment
 import com.discordtime.gpts.tools.Constants.TAG_LIST_PLACE_FRAGMENT
 import kotlinx.android.synthetic.main.activity_lobby.*
+import androidx.fragment.app.Fragment
+import com.discordtime.gpts.profile.ProfileFragment
+import com.discordtime.gpts.tools.Constants.TAG_PROFILE_FRAGMENT
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
 
 class LobbyActivity : AppCompatActivity() {
 
     lateinit var toolbar: ActionBar
     private val fragmentListPlaces = ListPlacesFragment()
+    private var profileFragment = ProfileFragment()
     private val fm: FragmentManager = supportFragmentManager
-    private var active = fragmentListPlaces
+    private var active: Fragment = fragmentListPlaces
 
-    private lateinit var textMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +31,6 @@ class LobbyActivity : AppCompatActivity() {
         toolbar = supportActionBar!!
         toolbar.title = "Map"
 
-        textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
         initComponents()
@@ -39,27 +41,30 @@ class LobbyActivity : AppCompatActivity() {
             fragmentListPlaces, TAG_LIST_PLACE_FRAGMENT)
             .hide(fragmentListPlaces).commit()
 
+        fm.beginTransaction().add(R.id.container,profileFragment,TAG_PROFILE_FRAGMENT)
+            .hide(profileFragment).commit()
     }
 
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    private val onNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_map -> {
                 toolbar.title = "Map"
-                textMessage.setText("Fragment 1")
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_list_places -> {
                 toolbar.title = "List Places"
-                fm.beginTransaction().show(fragmentListPlaces).commit()
+                fm.beginTransaction().hide(active).show(fragmentListPlaces).commit()
                 active = fragmentListPlaces
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_profile -> {
                 toolbar.title = "Profile"
-                textMessage.setText("Fragment 3")
+                fm.beginTransaction().hide(active).show(profileFragment).commit()
+                active = profileFragment
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
+
 }
--- app/src/main/java/com/discordtime/gpts/tools/Constants.kt
+++ app/src/main/java/com/discordtime/gpts/tools/Constants.kt
@@ -3,4 +3,5 @@ package com.discordtime.gpts.tools
 object Constants {
     //Value of list place fragment for botton view nav
     const val TAG_LIST_PLACE_FRAGMENT = "2"
+    const val TAG_PROFILE_FRAGMENT = "3"
 }
