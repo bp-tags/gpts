938be8addfb8e9f0c9cb381de21b6e2bdfe4816c
antoniosj <antonio.silvajr23@gmail.com>
2019-06-12 02:26:47 -0300

    Update listplaces activity to fragment

--- app/src/main/java/com/discordtime/gpts/lobby/LobbyActivity.kt
+++ app/src/main/java/com/discordtime/gpts/lobby/LobbyActivity.kt
@@ -5,33 +5,20 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.FragmentManager
 import com.discordtime.gpts.R
+import com.discordtime.gpts.listplaces.view.ListPlacesFragment
+import com.discordtime.gpts.tools.Constants.TAG_LIST_PLACE_FRAGMENT
+import kotlinx.android.synthetic.main.activity_lobby.*
 
 class LobbyActivity : AppCompatActivity() {
 
     lateinit var toolbar: ActionBar
+    private val fragmentListPlaces = ListPlacesFragment()
+    private val fm: FragmentManager = supportFragmentManager
+    private var active = fragmentListPlaces
 
     private lateinit var textMessage: TextView
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_map -> {
-                toolbar.title = "Map"
-                textMessage.setText("Fragment 1")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_list_places -> {
-                toolbar.title = "List Places"
-                textMessage.setText("Fragment 2")
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_profile -> {
-                toolbar.title = "Profile"
-                textMessage.setText("Fragment 3")
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,5 +30,36 @@ class LobbyActivity : AppCompatActivity() {
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
+        initComponents()
+    }
+
+    private fun initComponents() {
+        fm.beginTransaction().add(R.id.container,
+            fragmentListPlaces, TAG_LIST_PLACE_FRAGMENT)
+            .hide(fragmentListPlaces).commit()
+
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_map -> {
+                toolbar.title = "Map"
+                textMessage.setText("Fragment 1")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_list_places -> {
+                toolbar.title = "List Places"
+                fm.beginTransaction().show(fragmentListPlaces).commit()
+                active = fragmentListPlaces
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_profile -> {
+                toolbar.title = "Profile"
+                textMessage.setText("Fragment 3")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
     }
 }
---
+++ app/src/main/java/com/discordtime/gpts/tools/Constants.kt
@@ -0,0 +1,6 @@
+package com.discordtime.gpts.tools
+
+object Constants {
+    //Value of list place fragment for botton view nav
+    const val TAG_LIST_PLACE_FRAGMENT = "2"
+}
