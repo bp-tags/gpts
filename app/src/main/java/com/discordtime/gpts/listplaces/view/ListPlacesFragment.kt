eb15d753cf397b7a4d5170b852f0b97b65dc9c21
antoniosj <antonio.silvajr23@gmail.com>
2019-06-12 02:26:47 -0300

    Update listplaces activity to fragment

--- app/src/main/java/com/discordtime/gpts/listplaces/view/ListPlacesFragment.kt
+++ app/src/main/java/com/discordtime/gpts/listplaces/view/ListPlacesFragment.kt
@@ -1,34 +1,36 @@
 package com.discordtime.gpts.listplaces.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.discordtime.gpts.R
 import com.discordtime.gpts.listplaces.model.Place
 import com.discordtime.gpts.listplaces.viewmodel.ListPlacesViewModel
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_list_places.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class ListPlacesActivity : AppCompatActivity() {
+class ListPlacesFragment: Fragment() {
 
     private val listPlacesViewModel: ListPlacesViewModel by viewModel()
     private lateinit var listPlaces: List<Place>
     private lateinit var listPlacesAdapter: ListPlacesAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_list_places, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         listPlaces = listOf()
-        listPlacesAdapter = ListPlacesAdapter(this)
+        listPlacesAdapter = ListPlacesAdapter(view.context)
         listPlacesAdapter.listPlaces = listPlaces
 
         rv_places.adapter = listPlacesAdapter
-        rv_places.layoutManager = LinearLayoutManager(this)
+        rv_places.layoutManager = LinearLayoutManager(view.context)
 
         fetchPlaces()
     }
