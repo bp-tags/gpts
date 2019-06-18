add9563114c1259516c54e96bb9e463176b51640
Carlos Olimpio <carlossolimpio@hotmail.com>
2019-06-18 17:09:10 -0300

    Created the map fragment and added it to the lobby activity.

--- app/src/main/java/com/discordtime/gpts/lobby/LobbyActivity.kt
+++ app/src/main/java/com/discordtime/gpts/lobby/LobbyActivity.kt
@@ -1,33 +1,44 @@
 package com.discordtime.gpts.lobby
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import androidx.appcompat.app.ActionBar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.discordtime.gpts.R
 import com.discordtime.gpts.listplaces.view.ListPlacesFragment
+import com.discordtime.gpts.maps.view.MapViewFragment
+import com.discordtime.gpts.tools.Constants.PERMISSION_REQUEST_LOCATION_CODE
 import com.discordtime.gpts.tools.Constants.TAG_LIST_PLACE_FRAGMENT
+import com.discordtime.gpts.tools.Constants.TAG_MAPS_FRAGMENT
 import kotlinx.android.synthetic.main.activity_lobby.*
 
 class LobbyActivity : AppCompatActivity() {
 
     lateinit var toolbar: ActionBar
     private val fragmentListPlaces = ListPlacesFragment()
+    private val fragmentMaps = MapViewFragment()
     private val fm: FragmentManager = supportFragmentManager
-    private var active = fragmentListPlaces
+    private var active: Fragment = fragmentListPlaces
 
     private lateinit var textMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lobby)
+
+        //sets up location permission
+        setupPermissions()
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         toolbar = supportActionBar!!
-        toolbar.title = "Map"
-
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
@@ -35,31 +46,54 @@ class LobbyActivity : AppCompatActivity() {
     }
 
     private fun initComponents() {
-        fm.beginTransaction().add(R.id.container,
-            fragmentListPlaces, TAG_LIST_PLACE_FRAGMENT)
-            .hide(fragmentListPlaces).commit()
-
+        fm.beginTransaction().replace(R.id.container, fragmentMaps).commit()
     }
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_map -> {
-                toolbar.title = "Map"
-                textMessage.setText("Fragment 1")
+                toolbar.title = "Maps"
+                fm.beginTransaction().replace(R.id.container, fragmentMaps).commit()
+                active = fragmentMaps
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_list_places -> {
                 toolbar.title = "List Places"
-                fm.beginTransaction().show(fragmentListPlaces).commit()
+                fm.beginTransaction().replace(R.id.container, fragmentListPlaces).commit()
                 active = fragmentListPlaces
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_profile -> {
                 toolbar.title = "Profile"
-                textMessage.setText("Fragment 3")
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
+    }
+
+    //TODO check permissions
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permission != PackageManager.PERMISSION_GRANTED)
+            makeRequest()
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_LOCATION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when(requestCode) {
+            PERMISSION_REQUEST_LOCATION_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                } else {
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/discordtime/gpts/tools/Constants.kt
+++ app/src/main/java/com/discordtime/gpts/tools/Constants.kt
@@ -1,6 +1,12 @@
 package com.discordtime.gpts.tools
 
 object Constants {
-    //Value of list place fragment for botton view nav
+
+    //Value of each fragment for botton view nav
+    const val TAG_MAPS_FRAGMENT = "1"
     const val TAG_LIST_PLACE_FRAGMENT = "2"
+    const val TAG_PROFILE_FRAGMENT = "3"
+
+    //Value used for location permission
+    const val PERMISSION_REQUEST_LOCATION_CODE = 99
 }
