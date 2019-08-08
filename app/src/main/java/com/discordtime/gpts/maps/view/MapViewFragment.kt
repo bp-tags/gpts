a18dd2d81810391b703f6ff4a5da46b88ae96721
Carlos Olimpio <carlossolimpio@hotmail.com>
2019-08-08 17:27:54 -0300

    issue #19
    Defined and set app's icon and marker.

--- app/src/main/java/com/discordtime/gpts/maps/view/MapViewFragment.kt
+++ app/src/main/java/com/discordtime/gpts/maps/view/MapViewFragment.kt
@@ -1,19 +1,21 @@
 package com.discordtime.gpts.maps.view
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.discordtime.gpts.R
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.*
 
 class MapViewFragment : Fragment(), OnMapReadyCallback {
 
@@ -39,10 +41,30 @@ class MapViewFragment : Fragment(), OnMapReadyCallback {
 
         // For dropping a marker at a point on the Map
         val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"))
+        googleMap.addMarker(MarkerOptions()
+            .position(sydney)
+            .title("Marker Title")
+            .snippet("Marker Description")
+            .icon(generateBitmapDescriptorFromRes(context, R.drawable.ic_maker_poop)))
 
         // For zooming automatically to the location of the marker
         val cameraPosition = CameraPosition.Builder().target(sydney).zoom(12f).build()
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+    }
+
+    /**
+     * Method used for generating icon marker
+     */
+    private fun generateBitmapDescriptorFromRes(context: Context?, resId: Int): BitmapDescriptor {
+        val drawable = ContextCompat.getDrawable(context!!, resId)
+        drawable!!.setBounds(0,0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        val bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth,
+            drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
